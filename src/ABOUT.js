2ea81a2472eabc8e55a4a6c4704ec5d84fcb5789
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-01-31 11:19:48 +0400

    Update ABOUT.js

--- src/ABOUT.js
+++ src/ABOUT.js
@@ -8,7 +8,6 @@ export default function ABOUT() {
     <div className="img1"></div>
     <div className="p1">
         <p>
-            
             Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of 
             classNameical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin 
             professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, 
