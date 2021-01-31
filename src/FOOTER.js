73039c465619533985c7001b10fd40558a6b23c2
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-01-31 16:14:45 +0400

    Add files via upload

---
+++ src/FOOTER.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function FOOTER() {
+    return (
+        <>
+            <div className='footer'>
+                <div className='social-media-links'></div>
+                <div className='rights-reserved'>
+                    <div className='span1'>Created By Rabee Aljaramani</div>
+                    <div className='span2'>All Rights Reserved || 2021</div>
+                </div>
+            </div>
+        </>
+    )
+}
